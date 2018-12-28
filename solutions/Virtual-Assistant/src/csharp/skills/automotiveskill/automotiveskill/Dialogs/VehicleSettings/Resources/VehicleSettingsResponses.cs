38427139485b6cf32c6ea52921b1f424bace7d9b
Marion Zepf <mazepf@microsoft.com>
2018-12-28 11:28:57 -0500

    Automotive: Remove/change customer-specific bot responses

--- solutions/Virtual-Assistant/src/csharp/skills/automotiveskill/automotiveskill/Dialogs/VehicleSettings/Resources/VehicleSettingsResponses.cs
+++ solutions/Virtual-Assistant/src/csharp/skills/automotiveskill/automotiveskill/Dialogs/VehicleSettings/Resources/VehicleSettingsResponses.cs
@@ -30,13 +30,7 @@ namespace AutomotiveSkill.Dialogs.VehicleSettings.Resources
 
         public static BotResponse VehicleSettingsSettingValueSelection => GetBotResponse();
 
-        public static BotResponse VehicleSettingsSettingValueSelectionPre => GetBotResponse();
-
-        public static BotResponse VehicleSettingsSettingValueSelectionPost => GetBotResponse();
-
         public static BotResponse VehicleSettingsSettingChangeConfirmation => GetBotResponse();
-
-        public static BotResponse VehicleSettingsSettingChangeConfirmationWithCategory => GetBotResponse();
 
         public static BotResponse VehicleSettingsSettingChangeConfirmationDenied => GetBotResponse();
 
